90df1296d5aa55f1aad9e53c547360f9192d2ab9
amirali <yones.gholami45@gmail.com>
2016-07-02 20:06:28 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -259,7 +259,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {214795464}
   }
   save_config()
 end
